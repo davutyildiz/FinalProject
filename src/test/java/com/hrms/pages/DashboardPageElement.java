579883f727276a9e3c3d926af596d49696f08b45
bulentgeckin <bulentgeckin@gmail.com>
2020-02-08 16:34:20 -0500

    sample feature

--- src/test/java/com/hrms/pages/DashboardPageElement.java
+++ src/test/java/com/hrms/pages/DashboardPageElement.java
@@ -1,5 +1,17 @@
 package com.hrms.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.hrms.testbase.BaseClass;
+
 public class DashboardPageElement {
 
+	@FindBy(linkText = "PIM")
+	public WebElement pim;
+
+	public DashboardPageElement() {
+		PageFactory.initElements(BaseClass.driver, this);
+	}
 }
