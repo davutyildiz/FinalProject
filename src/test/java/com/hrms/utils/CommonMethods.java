1728ae71da00fb0b832e6c827dc3d31f8557afe6
bulentgeckin <bulentgeckin@gmail.com>
2020-02-08 14:42:41 -0500

    pages updated

--- src/test/java/com/hrms/utils/CommonMethods.java
+++ src/test/java/com/hrms/utils/CommonMethods.java
@@ -20,9 +20,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.hrms.testbase.BaseClass;
 import com.hrms.testbase.PageInitializeAll;
-import com.hrms.testbase.PageInitializeAll ;
 
 //command+o--> for mac to see all methods within the class
 //ctrl+o--> for windows
