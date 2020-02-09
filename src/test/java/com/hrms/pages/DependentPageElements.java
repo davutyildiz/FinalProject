37890b6838243f6715abab1ca57ca2ff01a172a8
bulentgeckin <bulentgeckin@gmail.com>
2020-02-09 15:43:31 -0500

    i commited my changes

--- src/test/java/com/hrms/pages/DependentPageElements.java
+++ src/test/java/com/hrms/pages/DependentPageElements.java
@@ -21,6 +21,15 @@ public class DependentPageElements extends CommonMethods{
 	@FindBy(id = "dependent_relationshipType")
 	public WebElement dependent_relationshipType;
 	
+	@FindBy(id = "dependent_dateOfBirth")
+	public WebElement dependent_dateOfBirth;
+	
+	@FindBy(id = "btnSaveDependent")
+	public WebElement btnSaveDependent;
+	
+	@FindBy(id = "dependent_relationship")
+	public WebElement dependent_relationship;
+	
 	
 	public DependentPageElements() {
 		PageFactory.initElements(BaseClass.driver, this);
